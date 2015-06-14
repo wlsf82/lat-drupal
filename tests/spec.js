cb744b3b1db819293c65a29a52d71e96cacc00c8
wlsf82 <wlsf82@gmail.com>
2015-06-14 04:50:25 -0300

    Added tests for login and logout.

--- tests/spec.js
+++ tests/spec.js
@@ -2,7 +2,8 @@
  * @file spec.js
  */
 
-var HomePage = require("./pages/home.page");
+var HomePage = require('./pages/home.page')
+	, helper = require('./helpers/helper');
 
 browser.ignoreSynchronization = true;
 
@@ -13,6 +14,16 @@ describe ('home page' , function () {
 	it ('should have a welcome message', function () {
 		browser.get('/');
 		expect(home.getWelcome()).toEqual('Welcome to lat-drupal.local');
+	});
+
+	it ('should login as admin', function () {
+		helper.login();
+		expect(element(by.css('body.logged-in')).isPresent()).toBe(true);
+	});
+
+	it ('should logout', function () {
+		helper.logout();
+		expect(element(by.css('body.logged-in')).isPresent()).toBe(false);
 	});
 
 });
