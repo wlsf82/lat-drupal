6fae09215ebdb17b1af0c162873f54954ccfcf94
wlsf82 <wlsf82@gmail.com>
2015-06-14 04:50:03 -0300

    Added helpers to login and logout.

---
+++ tests/helpers/helper.js
@@ -0,0 +1,28 @@
+var adminlogin = browser.params.adminLogin;
+
+function login () {
+	var user = adminlogin.user
+		, password = adminlogin.password;
+
+	browser.get('/');
+
+	var usernameField = element(by.css('#edit-name'))
+    , passwordField = element(by.css('#edit-pass'))
+    , submitButton = element(by.css('#edit-submit'));
+
+  usernameField.sendKeys(user).then(function() {
+    passwordField.sendKeys(password).then(function() {
+      submitButton.click();
+    });
+  });
+}
+
+function logout() {
+  element(by.cssContainingText('#toolbar-user .last a', 'Log out')).click();
+}
+
+// Exposed helper methods.
+module.exports = {
+  login: login,
+  logout: logout
+};
