f2576a44358fc7343d90a0a1446b69c1e966fa8a
wlsf82 <wlsf82@gmail.com>
2015-06-14 04:02:02 -0300

    First protractor configurations for lat-drupal project.

---
+++ tests/conf.js
@@ -0,0 +1,41 @@
+/**
+ * @file conf.js
+ */
+
+// var cloudTestingHelper = require('./helpers/cloud.testing.helper');
+
+exports.config = {
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+
+  specs: [ 'spec.js' ],
+
+  baseUrl: 'http://lat-drupal.local/',
+
+  capabilities: {
+
+    'browserName': 'chrome',
+
+    // Saucelabs credentials
+    // username: cloudTestingHelper.getInfo('saucelabs', 'user'),
+    // accessKey: cloudTestingHelper.getInfo('saucelabs', 'key'),
+
+    // Test suite name.
+    'name': 'lat-drupal test suite'
+
+  },
+
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true, // display summary of all failures after execution
+      displayFailedSpec: true,      // display each failed spec
+      displaySuiteNumber: true,     // display each suite number (hierarchical)
+      displaySpecDuration: true     // display each spec duration
+    }));
+  },
+
+}
