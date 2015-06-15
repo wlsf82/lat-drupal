3749c43923f3edf4a72002f3316fb66c137104d4
wlsf82 <wlsf82@gmail.com>
2015-06-14 23:01:23 -0300

    Added authentication tests to a separete spec file.

---
+++ tests/authentication.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @file authentication.spec.js
+ */
+
+var helper = require('./helpers/helper');
+
+browser.ignoreSynchronization = true;
+
+describe ('authentication' , function () {
+	
+	it ('should login as admin', function () {
+		helper.login();
+		expect(element(by.css('body.logged-in')).isPresent()).toBe(true);
+	});
+
+	it ('should logout', function () {
+		helper.logout();
+		expect(element(by.css('body.logged-in')).isPresent()).toBe(false);
+	});
+
+});
