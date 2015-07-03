1774443eee5c6ac4b5574a6f003e165754582c76
wlsf82 <wlsf82@gmail.com>
2015-07-03 02:54:42 -0300

    Removed test from media spec since this doesnt make sense anymore.

--- tests/media.spec.js
+++ tests/media.spec.js
@@ -9,21 +9,6 @@ var HomePage = require('./pages/home.page')
 browser.ignoreSynchronization = true;
 
 describe ('media page' , function () {
-	
-	it ('should have sub-items into media menu for each kind of available media', function () {
-
-		var subItems = ['Hands-on videos and webinars', 'Podcasts', 'eBooks', 'Blog posts', 'Presentations', 'Pictures'];
-
-		HomePage.get();
-
-		helper.clickMenu('media');
-
-		subItems.forEach(function (subItem) {
-			expect(helper.getMenuSubItems()).toContain(subItem);
-		});
-
-	});
-
 	it ('should go to a list of media when accessing the media main page', function () {
 
 	});
