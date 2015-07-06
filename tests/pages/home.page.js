bd201144ab491b6a320e6d146ac1e6d24edaf9c0
wlsf82 <wlsf82@gmail.com>
2015-07-06 19:56:19 -0300

    Added new attribute and method on home page object to get title.

--- tests/pages/home.page.js
+++ tests/pages/home.page.js
@@ -6,9 +6,14 @@ var HomePage = function () {
 
 	this.mainMenu = element(by.css('#main-menu-links'));
 	this.contentContainer = element(by.css('#content'));
+	this.title = element(by.css('#site-name span'));
 
   this.get = function () {
   	browser.get('/');
+  };
+
+  this.getTitle = function () {
+  	return this.title.getText();
   };
 
 };
