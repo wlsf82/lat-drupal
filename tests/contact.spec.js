457599d5ad09d7253bcfb3dab37c67e00e5acf9d
wlsf82 <wlsf82@gmail.com>
2015-07-07 01:16:18 -0300

    Updated test for contact page and added new test for the same page. Update to adjust method usage that is now more generic; and new test for not successfully form submition because of missing mandatory fields.

--- tests/contact.spec.js
+++ tests/contact.spec.js
@@ -9,7 +9,13 @@ browser.ignoreSynchronization = true;
 describe ('Contact' , function () {
 	it ('should successfully submit the contact form', function () {
 		ContactPage.get();
-		ContactPage.successfullySubmit();
+		ContactPage.submit('Ana', 'ana@banana.com', 'teste lorem ipsum');
 		expect(ContactPage.successMessage.isDisplayed()).toBe(true);
+	});
+
+	it ('should not successfully submit the contact form because of empty mandatory fields', function () {
+		ContactPage.get();
+		ContactPage.submit('', '', '');
+		expect(ContactPage.successMessage.isPresent()).toBe(false);
 	});
 });
