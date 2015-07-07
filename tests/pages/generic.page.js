e4d33e873a2fdeb4eb1ea6b9efea1573f37b8e95
wlsf82 <wlsf82@gmail.com>
2015-07-06 21:25:17 -0300

    Updated generic page object to add the getBlogUrl mehotd.

--- tests/pages/generic.page.js
+++ tests/pages/generic.page.js
@@ -8,6 +8,10 @@ var GenericPage = function () {
   	return element(by.css('#content h1')).getText();
   };
 
+  this.getBlogUrl = function () {
+  	return element(by.css('#main-menu-links li:nth-child(4) a')).getAttribute('href');
+  };
+
 };
 
 module.exports = new GenericPage();
