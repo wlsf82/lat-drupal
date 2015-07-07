cadb479a7a5ea58a54fc8a4a2ea47ee500442a5a
wlsf82 <wlsf82@gmail.com>
2015-07-06 21:03:29 -0300

    LAT-7: Updated tests for media page.

--- tests/media.spec.js
+++ tests/media.spec.js
@@ -3,18 +3,20 @@
  */
 
 var HomePage = require('./pages/home.page')
-	// , MediaPage = require('./pages/media.page')
-	, helper = require('./helpers/helper');
+	, MediaPage = require('./pages/media.page')
+	, genericPage = require('./pages/generic.page');
 
 browser.ignoreSynchronization = true;
 
-describe ('media page' , function () {
-	it ('should go to a list of media when accessing the media main page', function () {
-
+describe ('Media' , function () {
+	it ('should have a title', function () {
+		MediaPage.get();
+		expect(genericPage.getContentTitle()).toEqual('Midias');
 	});
 
-	it ('should go to a list of specific kind of media', function () {
-
+	it ('should have a list of media', function () {
+		MediaPage.get();
+		expect(MediaPage.viewList.isPresent()).toBe(true);
 	});
 
 });
